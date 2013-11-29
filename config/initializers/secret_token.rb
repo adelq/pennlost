caba97c1c24cb32141328be1171b5138721bd1de
Adel Qalieh <aqalieh95@gmail.com>
2013-11-28 23:16:36 -0500

    Initialize Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pennlost::Application.config.secret_key_base = '50b58f714a09f288a25014d29b226edcc0ae3828588a3a0c998016f8bbaf8ff3bffd7619fc42ba7e5b66e2ef3268a6e2054a0cf9c021d10594759edd6ce229b1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pennlost::Application.config.session_store :cookie_store, key: '_pennlost_session'
