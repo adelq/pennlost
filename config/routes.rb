0950e20d74468a73e132738ed10b1947b6ea7927
Adel Qalieh <aqalieh95@gmail.com>
2013-11-29 19:25:06 -0500

    Add static pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Pennlost::Application.routes.draw do
+  get "static/home"
+  get "static/help"
+  get "static/about"
+  get "static/contact"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
